38d93aa043e31ef216c553daee95108948c2de90
JunW62 <sivia1990@gmail.com>
2024-06-04 10:43:28 -0500

    third commit

--- feelLoopy.js
+++ feelLoopy.js
@@ -1,5 +1,5 @@
 var str =
-  "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,QuizMaster,58\n98,Bill,Doctor’s Assistant,26";
+  "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,QuizMaster,58\n98,Bill,Doctor's Assistant,26";
 var cells = [];
 cell = "";
 for (i = 0; i < str.length; i++) {
@@ -10,6 +10,7 @@ for (i = 0; i < str.length; i++) {
   }
   cell += str[i];
 }
+cells.push(cell);
 for (i = 0; i < cells.length / 4; i++) {
   console.log(
     cells[4 * i],
